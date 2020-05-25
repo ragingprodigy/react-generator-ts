f60f3b4be6bbb62ddd3520f54a1b10d36a2dc5a4
Oladapo Omonayajo <o.omonayajo@gmail.com>
2020-05-26 07:45:23 +0800

    Added support templates for styled components

--- src/templates/component.template.ts
+++ src/templates/component.template.ts
@@ -1,11 +1,30 @@
 export const getComponentTemplate = (componentName: string) => {
   return `import React, { Component } from 'react';
 
-export class ${componentName} extends Component {
+export interface ${componentName}Props {
+  title: string;
+}
+
+export class ${componentName} extends Component<${componentName}Props> {
   render() {
-    return <div></div>;
+    return <div>{this.props.title}</div>;
   }
 }
+
+export default ${componentName};
+`;
+};
+
+export const getFunctionalComponentTemplate = (componentName: string) => {
+  return `import React from 'react';
+
+export interface ${componentName}Props {
+  title: string;
+}
+
+export const ${componentName} = (props: ${componentName}Props) => {
+  return <div>{props.title}</div>;
+};
 
 export default ${componentName};
 `;
--- src/templates/styled-component.template.ts
+++ src/templates/styled-component.template.ts
@@ -2,11 +2,35 @@ export const getStyledComponentTemplate = (componentName: string, useSCSS: boole
   return `import React, { Component } from 'react';
 import styles from './${componentName}.module.${useSCSS ? 'scss' : 'css'}';
 
-export class ${componentName} extends Component {
+export interface ${componentName}Props {
+  title: string;
+}
+
+export class ${componentName} extends Component<${componentName}Props> {
   render() {
-    return <div className={styles.${componentName}}></div>;
+    return <div className={styles.${componentName}}>
+      {this.props.title}
+    </div>;
   }
 }
+
+export default ${componentName};
+`;
+};
+
+export const getStyledFunctionalComponentTemplate = (componentName: string, useSCSS: boolean) => {
+  return `import React from 'react';
+import styles from './${componentName}.module.${useSCSS ? 'scss' : 'css'}';
+
+export interface ${componentName}Props {
+  title: string;
+}
+
+export const ${componentName} = (props: ${componentName}Props) => {
+  return <div className={styles.${componentName}}>
+    {props.title}
+  </div>;
+};
 
 export default ${componentName};
 `;
